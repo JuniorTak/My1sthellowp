de448938c2366cacb0950ebaa7a4f60b0c1260e9
Hyppolite <hyppolitak@gmail.com>
2024-04-15 15:37:26 +0100

    Apply PHP code sniffer corrections for WP

--- My1sthellowp.php
+++ My1sthellowp.php
@@ -3,6 +3,7 @@
  * @package My1sthellowp
  * @version 1.0.0
  */
+
 /*
 Plugin Name: My1sthellowp
 Plugin URI: https://github.com/JuniorTak/My1sthellowp
@@ -13,14 +14,23 @@ Author: Hyppolite T.
 Author URI: https://www.hyppolitetakouafoduop.online
 */
 
-/* Shortcode – Google Maps Integration */
-function fn_googleMaps($atts, $content = null) {
-   extract(shortcode_atts(array(
-      "width" => '100%',
-      "height" => '420',
-      "src" => 'https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d1989.9103126465243!2d9.7206582!3d4.0569695!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2scm!4v1690665491625!5m2!1sen!2scm'
-   ), $atts));
-   return '<iframe width="' . $width . '" height="' . $height . '" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="' . $src . '&amp;output=embed"></iframe>';
+/**
+ * Shortcode – Google Maps Integration.
+ *
+ * @param array $atts the shortcode attributs.
+ * @return string
+ */
+function fn_google_maps( $atts ) {
+	extract(
+		shortcode_atts(
+			array(
+				'width'  => '100%',
+				'height' => '420',
+				'src'    => 'https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d1989.9103126465243!2d9.7206582!3d4.0569695!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2scm!4v1690665491625!5m2!1sen!2scm',
+			),
+			$atts
+		)
+	);
+	return '<iframe width="' . $width . '" height="' . $height . '" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="' . $src . '&amp;output=embed"></iframe>';
 }
-add_shortcode("googlemap", "fn_googleMaps") ;
-?>
+add_shortcode( 'googlemap', 'fn_google_maps' );
